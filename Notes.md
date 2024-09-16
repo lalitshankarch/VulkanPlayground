5e9e6d3560415f7934b9bfc1a9adde0ffe7b87c6
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2024-09-16 10:10:57 +0530

    HelloTriangle: Clear color

--- Notes.md
+++ Notes.md
@@ -32,7 +32,9 @@ High level overview of rendering a model:
 
     (The steps above can be automated by `vk-bootstrap`)
 
-    - Load model and setup vertex and index buffers and shaders
+    - Read SPIR-V shaders and create shader modules
+    - Create graphics pipeline from the shader modules
+    - Load model and setup vertex and index buffers
     - Get next swapchain image
     - Record and finish commands
     - Present current swapchain image
@@ -40,6 +42,8 @@ High level overview of rendering a model:
     - Cleanup all allocated resources
 
 Vulkan validation layers facilitate proper API usage.
+
+The graphics pipeline is the sequence of operations that take the vertices and textures of the meshes all the way to the pixels in the render targets.
 
 APIs like `OpenGL` and `DX 11` calculate pipeline objects automatically, while Vulkan enables the programmer to cache pipeline objects or generate them "on the fly" while rendering. Creating pipelines are expensive, so it's best to create them on a background thread or on a loading screen.
 
@@ -61,4 +65,63 @@ The general flow to execute commands is:
 
 Multiple threads can record commands into a single queue but only one thread should submit them to a queue. Also, each thread should use its own copy of a command allocator and command buffer.
 
-The graphics pipeline is the sequence of operations that take the vertices and textures of the meshes all the way to the pixels in the render targets.
+In Vulkan, all of the rendering happens inside a `VkRenderPass`. It's possible to do compute commands outside of a renderpass. `VkRenderPass` is an object that encapsulates the state needed to setup the target for rendering, and the state of the images we will be rendering to.
+
+A `VkRenderPass` renders into a `VkFramebuffer`. It allows to driver to know more about the state of images presenting more opportunities for optimization. The framebuffer links to the images we will render to, and it's used when starting a renderpass to set the target images for rendering.
+
+The general use of a renderpass when encoding commands is like this:
+
+```
+vkBeginCommandBuffer(cmd, ...);
+
+vkCmdBeginRenderPass(cmd, ...);
+
+//rendering commands go here
+
+vkCmdEndRenderPass(cmd);
+
+vkEndCommandBuffer(cmd)
+```
+
+A renderpass consists of subpasses, which are a bit like rendering steps. A renderpass should contain a minimum of 1 subpass.
+
+A renderpass performs image layout changes when entering and exiting the renderpass. For optimization purposes, GPUs compress textures or reorder pixels so that they mipmap better.
+
+The color attachment is the description of the image we will be writing into with rendering commands.
+
+Vulkan offers explicit synchronization structures to allow the CPU to sync execution of commands with the GPU.
+
+`VkFence` is used for GPU -> CPU synchronization. If this is set, we can know from the CPU if the GPU has finished these operations.
+
+Pseudocode:
+
+```
+VkFence myFence;
+
+//start some operation on the GPU
+VkSomeOperation(whatever, myFence);
+
+// block the CPU until the GPU operation finishes
+VkWaitForFences(myFence);
+
+//fences always have to be reset before they can be used again
+VkResetFences(myFence);
+```
+
+For GPU - GPU synchronization, `VkSemaphore` is used. Semaphores allow defining order of operations on GPU commands.
+
+Requesting an image from the swapchain will block the CPU thread until the image is available. Using V-sync'd modes block the CPU for long while modes like mailbox return almost immediately.
+
+Request image -> Reset command buffer -> Start rendering commands -> End rendering commands -> Submit to graphics queue -> Wait for fence -> Present -> Reset fence.
+
+Unlike older APIs, shader code in Vulkan has to be specified in a bytecode format as opposed to human readable syntax like GLSL/HLSL. The bytecode, called SPIR-V (Standard, Portable Intermediate Representation).
+
+The vertex shader runs for each vertex. It takes attributes like color, world position, normal and texture coordinates as input. The output is a final position in clip coordinates andattributes that need to be passed on to the fragment shader. The rasterizer will then interpolate the values to produce a smooth gradient.
+
+The vertex shader receives coordinates in Normalized Device Coordinates (NDC) format. The origin is in the center, the Y axis is flipped, and Z ranges from 0 to 1 (transformed from -1 to 1).
+
+We can directly output normalized device coordinates by outputting them as clip coordinates from the vertex shader with the last component set to 1. That way the division to transform clip coordinates to normalized device coordinates does not change anything.
+
+`gl_Position` is a built in variable storing the final position output of the vertex shader.
+
+`gl_VertexIndex` stores the current vertex index.
--- src/HelloTriangle/main.cpp
+++ src/HelloTriangle/main.cpp
@@ -3,11 +3,12 @@
 #include <vulkan/vulkan.hpp>
 #include <VkBootstrap.h>
 #include <spdlog/spdlog.h>
+#include <fstream>
 
 const uint32_t width = 640, height = 480;
 
 // Throws std::runtime_error on nullptr or falsy values
-void check(const auto &ptr, const std::string &msg)
+static void check(const auto &ptr, const std::string &msg)
 {
 #ifdef NDEBUG
     (void)ptr;
@@ -27,6 +28,7 @@ void check(const auto &ptr, const std::string &msg)
 #define main() WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #endif
 #endif
+
 int main()
 {
     vkb::InstanceBuilder builder{};
@@ -57,7 +59,7 @@ int main()
 
     vkb::PhysicalDeviceSelector phys_dev_selector{vkb_inst};
     auto phys_ret = phys_dev_selector.set_surface(surface)
-                        .set_minimum_version(1, 3)
+                        .set_minimum_version(1, 2)
                         .select();
     check(phys_ret, "Vulkan: Failed to select physical device");
 
@@ -65,6 +67,7 @@ int main()
     auto dev_ret = dev_builder.build();
     check(dev_ret, "Vulkan: Failed to create logical device");
     vkb::Device vkb_dev = dev_ret.value();
+    vk::Device device = vkb_dev.device;
 
     auto gr_queue_ret = vkb_dev.get_queue(vkb::QueueType::graphics);
     check(gr_queue_ret, "Vulkan: Failed to get graphics queue");
@@ -78,11 +81,142 @@ int main()
     auto images = vkb_swapchain.get_images().value();
     auto image_views = vkb_swapchain.get_image_views().value();
 
+    vk::CommandPoolCreateInfo create_info{};
+    create_info.queueFamilyIndex = vkb_dev.get_queue_index(vkb::QueueType::graphics).value();
+    create_info.flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer;
+    vk::CommandPool command_pool{};
+    check(device.createCommandPool(&create_info, nullptr, &command_pool) == vk::Result::eSuccess, "Failed to create command pool");
+
+    vk::CommandBufferAllocateInfo alloc_info{};
+    alloc_info.commandBufferCount = 1;
+    alloc_info.commandPool = command_pool;
+    alloc_info.level = vk::CommandBufferLevel::ePrimary;
+
+    vk::CommandBuffer command_buffer{};
+    check(device.allocateCommandBuffers(&alloc_info, &command_buffer) == vk::Result::eSuccess, "Failed to allocate command buffer");
+
+    vk::AttachmentDescription color_attachment{};
+    color_attachment.format = static_cast<vk::Format>(vkb_swapchain.image_format);
+    color_attachment.samples = vk::SampleCountFlagBits::e1;
+    color_attachment.loadOp = vk::AttachmentLoadOp::eClear;
+    color_attachment.storeOp = vk::AttachmentStoreOp::eStore;
+    color_attachment.stencilLoadOp = vk::AttachmentLoadOp::eDontCare;
+    color_attachment.stencilStoreOp = vk::AttachmentStoreOp::eDontCare;
+    color_attachment.initialLayout = vk::ImageLayout::eUndefined;
+    color_attachment.finalLayout = vk::ImageLayout::ePresentSrcKHR;
+
+    vk::AttachmentReference color_attachment_ref{};
+    color_attachment_ref.attachment = 0;
+    color_attachment_ref.layout = vk::ImageLayout::eColorAttachmentOptimal;
+
+    vk::SubpassDescription subpass_desc{};
+    subpass_desc.pipelineBindPoint = vk::PipelineBindPoint::eGraphics;
+    subpass_desc.colorAttachmentCount = 1;
+    subpass_desc.pColorAttachments = &color_attachment_ref;
+
+    vk::RenderPassCreateInfo rp_create_info{};
+    rp_create_info.attachmentCount = 1;
+    rp_create_info.pAttachments = &color_attachment;
+    rp_create_info.subpassCount = 1;
+    rp_create_info.pSubpasses = &subpass_desc;
+
+    vk::RenderPass render_pass = device.createRenderPass(rp_create_info);
+    check(render_pass, "Failed to create render pass");
+
+    vk::FramebufferCreateInfo fb_create_info{};
+    fb_create_info.renderPass = render_pass;
+    fb_create_info.attachmentCount = 1;
+    fb_create_info.width = width;
+    fb_create_info.height = height;
+    fb_create_info.layers = 1;
+
+    std::vector<vk::Framebuffer> framebuffers{};
+    for (vk::ImageView image_view : image_views)
+    {
+        fb_create_info.pAttachments = &image_view;
+        vk::Framebuffer frame_buffer = device.createFramebuffer(fb_create_info);
+        check(frame_buffer, "Failed to create framebuffer");
+        framebuffers.emplace_back(frame_buffer);
+    }
+
+    vk::FenceCreateInfo fc_create_info{};
+    fc_create_info.flags = vk::FenceCreateFlagBits::eSignaled;
+
+    vk::Fence fence = device.createFence(fc_create_info);
+    check(fence, "Failed to create fence");
+
+    vk::SemaphoreCreateInfo sm_create_info{};
+
+    vk::Semaphore render_semaphore{}, present_semaphore{};
+    render_semaphore = device.createSemaphore(sm_create_info);
+    check(render_semaphore, "Failed to create render semaphore");
+    present_semaphore = device.createSemaphore(sm_create_info);
+    check(present_semaphore, "Failed to create present semaphore");
+
+    vk::CommandBufferBeginInfo cmd_begin_info{};
+    cmd_begin_info.flags = vk::CommandBufferUsageFlagBits::eSimultaneousUse;
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+
+        device.waitForFences(1, &fence, true, UINT64_MAX);
+        device.resetFences(1, &fence);
+
+        uint32_t swapchain_img_idx{};
+        device.acquireNextImageKHR(vkb_swapchain.swapchain, UINT64_MAX, present_semaphore, VK_NULL_HANDLE, &swapchain_img_idx);
+
+        command_buffer.reset();
+
+        command_buffer.begin(cmd_begin_info);
+
+        vk::ClearValue clear_value{};
+        clear_value.color = {0.39f, 0.58f, 0.93f, 1.0f};
+
+        vk::RenderPassBeginInfo rp_begin_info{};
+        rp_begin_info.renderPass = render_pass;
+        rp_begin_info.renderArea.extent.width = width;
+        rp_begin_info.renderArea.extent.height = height;
+        rp_begin_info.framebuffer = framebuffers[swapchain_img_idx];
+        rp_begin_info.clearValueCount = 1;
+        rp_begin_info.pClearValues = &clear_value;
+
+        command_buffer.beginRenderPass(&rp_begin_info, vk::SubpassContents::eInline);
+        command_buffer.endRenderPass();
+        command_buffer.end();
+
+        vk::SubmitInfo submit_info{};
+        submit_info.waitSemaphoreCount = 1;
+        submit_info.pWaitSemaphores = &present_semaphore;
+        vk::PipelineStageFlags wait_stage = vk::PipelineStageFlagBits::eColorAttachmentOutput;
+        submit_info.pWaitDstStageMask = &wait_stage;
+        submit_info.signalSemaphoreCount = 1;
+        submit_info.pSignalSemaphores = &render_semaphore;
+        submit_info.commandBufferCount = 1;
+        submit_info.pCommandBuffers = &command_buffer;
+
+        graphics_queue.submit(1, &submit_info, fence);
+
+        vk::PresentInfoKHR present_info{};
+        present_info.waitSemaphoreCount = 1;
+        present_info.pWaitSemaphores = &render_semaphore;
+        present_info.swapchainCount = 1;
+        present_info.pSwapchains = reinterpret_cast<vk::SwapchainKHR *>(&vkb_swapchain.swapchain);
+        present_info.pImageIndices = &swapchain_img_idx;
+
+        graphics_queue.presentKHR(&present_info);
     }
 
+    device.waitIdle();
+    for (auto &frame_buffer : framebuffers)
+    {
+        device.destroyFramebuffer(frame_buffer);
+    }
+    device.destroySemaphore(present_semaphore);
+    device.destroySemaphore(render_semaphore);
+    device.destroyFence(fence);
+    device.destroyRenderPass(render_pass);
+    device.destroyCommandPool(command_pool);
     vkb_swapchain.destroy_image_views(image_views);
     vkb::destroy_swapchain(vkb_swapchain);
     vkb::destroy_device(vkb_dev);
